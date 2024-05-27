c440254a8fc2dc008d0d1e5db220599fee7bb0a5
Nisha Subedi <nishasubedi61@gmail.com>
2024-05-27 13:52:16 +0545

    mini projects

---
+++ dicenozero.py
@@ -0,0 +1,51 @@
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    return random.randint(min_value, max_value)
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Number of players must be between 2 and 4.")
+    else:
+        print("Invalid input, please enter a number.")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print(f"\nPlayer {player_idx + 1}'s turn has just started!\n")
+        current_score = 0
+        
+        while True:
+            should_roll = input("Would you like to roll (y/n)? ")
+            if should_roll.lower() != "y":
+                break
+            
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                break
+            else:
+                current_score += value
+                print(f"You rolled a: {value}")
+                print(f"Your current score for this turn is: {current_score}")
+        
+        player_scores[player_idx] += current_score
+        print(f"Your total score is: {player_scores[player_idx]}")
+        
+        if player_scores[player_idx] >= max_score:
+            print(f"Player {player_idx + 1} wins with a score of {player_scores[player_idx]}!")
+            break
+    
+    if max(player_scores) >= max_score:
+        break
+
+print("Game over!")
---
+++ menu.py
@@ -0,0 +1,40 @@
+menu ={
+    'Pizza': 400,
+    'coffee': 140,
+    'Salad': 430,
+    'Pasta': 540,  
+}
+
+print("Welome")
+print("Pizza: RS400\ncoffee: Rs140\nSalad: Rs430\nPasta: Rs540")
+order_total = 0
+
+item_1 = input("Enter item you want to order = ")
+if item_1 in menu:
+    order_total += menu[item_1]
+    print(f"Your item {item_1} has been added to your order")
+else:
+    print(f"Ordered iten not available! ") 
+    
+another_order=input("Do you want to order more? (yes/no)")
+if another_order == "yes" :
+    item_2 = input("Enter the second order= ")
+    if item_2 in menu:
+        order_total += menu[item_2]
+        print(f"item{item_2} has been added to your order")
+    else:
+        print(f"ordered item{item_2}is not available!")   
+
+    
+another_order=input("Do you want to order more? (yes/no)")
+if another_order == "yes" :
+    item_3 = input("Enter the third order= ")
+    if item_3 in menu:
+        order_total += menu[item_3]
+        print(f"item{item_3} has been added to your order")
+    else:
+        print(f"ordered item{item_3}is not available!")   
+                
+        
+        
+print (f"The total amount of your order is{order_total}")
