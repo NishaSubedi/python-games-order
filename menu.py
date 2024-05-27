6ee0606d5c0dea8bfa2f03caf415542e5ec710fa
Nisha Subedi <nishasubedi61@gmail.com>
2024-05-27 13:56:15 +0545

    mini projects

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
-menu ={
+menu = {
     'Pizza': 400,
     'coffee': 140,
     'Salad': 430,
-    'Pasta': 540,  
+    'Pasta': 540,
 }
 
 print("Welome")
@@ -14,27 +14,26 @@ if item_1 in menu:
     order_total += menu[item_1]
     print(f"Your item {item_1} has been added to your order")
 else:
-    print(f"Ordered iten not available! ") 
-    
-another_order=input("Do you want to order more? (yes/no)")
-if another_order == "yes" :
+    print(f"Ordered iten not available! ")
+
+another_order = input("Do you want to order more? (yes/no)")
+if another_order == "yes":
     item_2 = input("Enter the second order= ")
     if item_2 in menu:
         order_total += menu[item_2]
         print(f"item{item_2} has been added to your order")
     else:
-        print(f"ordered item{item_2}is not available!")   
+        print(f"ordered item{item_2}is not available!")
 
-    
-another_order=input("Do you want to order more? (yes/no)")
-if another_order == "yes" :
+
+another_order = input("Do you want to order more? (yes/no)")
+if another_order == "yes":
     item_3 = input("Enter the third order= ")
     if item_3 in menu:
         order_total += menu[item_3]
         print(f"item{item_3} has been added to your order")
     else:
-        print(f"ordered item{item_3}is not available!")   
-                
-        
-        
-print (f"The total amount of your order is{order_total}")
+        print(f"ordered item{item_3}is not available!")
+
+
+print(f"The total amount of your order is{order_total}")
